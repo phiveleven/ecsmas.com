2fae8ab3248a6aecf1688d069a49b7c83a660e07
Francisco Brito <francisco.brito@gmail.com>
2010-12-24 22:32:13 -0800

    Ecsmas Flake :)

---
+++ web/js/phlaky.js
@@ -0,0 +1,75 @@
+/**
+ * Ye olde Ecsmas Phlake
+ * 
+ * @darkgoyle
+ * circa 2010
+ */
+var flake = (function(canvas){
+  
+  // that's right, these are globals, fools.
+  Phi=.5+Math.sqrt(5)/2;
+  TwoPi=2*Math.PI;
+  
+  var ctx = canvas.getContext('2d');
+  
+  // re-center coordinates
+  ctx.translate(canvas.width/2, canvas.height/2);
+  // serendipitous rotational offset
+  ctx.rotate(TwoPi*Math.random());
+  
+  var brush = 161;
+  var r_max = Math.min(canvas.width/2, canvas.height/2) - brush;
+  
+  // hexagonal point
+  function point(p, r){
+    r = r || brush;
+    var  x = -r/2,
+         y = (-r/2)/Phi;
+    ctx.save();
+    ctx.translate(p[0], p[1]);
+    var sym = 3,
+        i = sym;
+    while (i){
+      ctx.fillRect(x, y, r, r/Phi);
+      ctx.rotate(TwoPi/sym);
+      i--;
+    }
+    ctx.restore();
+  }
+  
+  // canvas API
+  return {
+    clear: function(){
+      var r = r_max*r_max;
+      ctx.clearRect(-r, -r, 2*r, 2*r) 
+    },
+    rotate: function(x){ ctx.rotate(TwoPi/x) },
+    point: point,
+    flake: function(n){
+      ctx.save();
+      var r = r_max, 
+          red,
+          x = Math.cos(TwoPi/12*(n+1)),
+          y = Math.sin(TwoPi/12*(n+1));
+      while(r > 0){
+        for (var i=0; i<6; i++){
+          
+          red = Math.floor(61 * (r/r_max));
+          ctx.fillStyle = 'rgba($r,$g,$b,$a)'.replace(/\$(\w)/g, function($0, $1){
+            return { r: red, g: 255-red, b:255, a: .06 }[$1];
+          });
+          
+          point([r, 0], brush*n);
+          for (var s = (r + r*n/2); s > r; s-=brush){
+            point([x*s, -y*s], brush/Phi);
+            point([x*s, y*s], brush/Phi);
+          }
+          
+          ctx.rotate(TwoPi/6);
+        }
+        r -= brush/Phi;
+      }
+      ctx.restore();
+    }
+  }
+})(document.getElementsByTagName('canvas')[0]);
