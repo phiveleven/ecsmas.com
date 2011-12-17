6b702f1af6d4d7fffb201de0366726c6fce8be4f
Jason Mooberry <jasonmoo@me.com>
2011-12-17 14:10:51 -0500

    perf changes

--- 2011/js/phlaky.js
+++ 2011/js/phlaky.js
@@ -26,62 +26,53 @@ var phlake = function(num, x,y, context, key){
   // brush width relative to canvas size
   var brush = canvas.width/canvas.height*91;
   var r_max = Math.min(canvas.width/2, canvas.height/2) - brush;
-
+  
+  var sym = 3,
+    TWOPIsym = TWOPI/sym;
   // hexagonal point
   function point(p, r){
-    r = r || brush;
     var  x = -r/2,
-         y = (-r/2)/PHI;
+         y = x/PHI;
     ctx.save();
     ctx.translate(p[0], p[1]);
-    var sym = 3,
-        i = sym;
-    while (i){
-      ctx.fillRect(x, y, r, r/PHI);
-      ctx.rotate(TWOPI/sym);
+    var i = sym,
+        rPHI = r/PHI;
+    while (i) {
+      ctx.fillRect(x, y, r, rPHI);
+      ctx.rotate(TWOPIsym);
       i--;
     }
     ctx.restore();
   }
 
   // phlake API
-  var phlake = {
-    clear: function(){
-      var r = 1000;
-      ctx.clearRect(-r, -r, 2*r, 2*r);
-    },
-    rotate: function(x){ ctx.rotate(TWOPI/x) },
-    point: point,
-    flake: function(n){
-      ctx.save();
-      var r = r_max,
-          red,
-          x = Math.cos(TWOPI/12*(n+1)),
-          y = Math.sin(TWOPI/12*(n+1));
-      while(r > 0){
-        for (var i=0; i<6; i++){
-
-          red = Math.floor(100 * (r/(r_max)));
-          ctx.fillStyle = 'rgba($r,$g,$b,$a)'.replace(/\$(\w)/g, function($0, $1){
-            return { r: red, g: 255-red, b:255, a: 15/r/PHI }[$1];
-          });
-
-          point([r, 0], brush*n);
-          for (var s = (r + r*n/2); s > r; s-=150*n){
-            point([x*s, -y*s], brush/PHI);
-            point([x*s, y*s], brush/PHI);
-          }
-
-          ctx.rotate(TWOPI/6);
+  var phlake = function(n){
+    var r = r_max,
+        red,
+        x = Math.cos(TWOPI/12*(n+1)),
+        y = Math.sin(TWOPI/12*(n+1)),
+        brushPHI = brush/PHI,
+        twoPI6 = TWOPI/6,
+        brushn = brush*n,
+        n150 = 150*n;
+    while(r > 0){
+      red = Math.floor(100 * (r/(r_max)));
+      ctx.fillStyle = 'rgba('+red+','+(255-red)+',255,'+(15/r/PHI)+')';
+      for (var i=0; i<6; i++){
+        point([r, 0], brushn);
+        for (var s = (r + r*n/2); s > r; s-=n150){
+          point([x*s, -y*s], brushPHI);
+          point([x*s, y*s], brushPHI);
         }
-        r -= brush/PHI;
+
+        ctx.rotate(twoPI6);
       }
-      ctx.restore();
+      r -= brushPHI;
     }
   }
 
-  phlake.flake(n);
-  phlake.flake(n/Math.PI*PHI);
-  phlake.flake(n/Math.PI/PHI);
+  phlake(n);
+  phlake(n/Math.PI*PHI);
+  phlake(n/Math.PI/PHI);
   ctx.restore();
 };
